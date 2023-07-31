5182bdd4ccf1af5b9f2609bee03be161a9e8e53b
yokoso <tomsolga90@gmail.com>
2023-07-31 21:06:45 +0200

    C - Item add itemTextCol container

--- app/[searchTerm]/components/Item.tsx
+++ app/[searchTerm]/components/Item.tsx
@@ -6,7 +6,16 @@ type Props = {
 }
 
 export default function Item({ result }: Props) {
-  return (
-    <div>Item</div>
+  const itemTextCol = (
+    <div className='flex flex-col justify-center'>
+        <h2>
+            <Link href={ `https://en.wikipedia.org/?curid=${result.pageid}` }   target='_blank'
+            className='text-xl font-bold underline'
+            >
+                { result.title }
+            </Link>
+        </h2>
+        <p>{ result.extract }</p>
+    </div>
   )
 }
