8c80d9c8355c7fc3f67fe35713d4400e0edccc93
yokoso <tomsolga90@gmail.com>
2023-07-31 21:00:53 +0200

    C - Item init

---
+++ app/[searchTerm]/components/Item.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+type Props = {
+    result: Result
+}
+
+export default function Item({ result }: Props) {
+  return (
+    <div>Item</div>
+  )
+}
